f3b7e516cc08eb477270aab41e3ac494fada5d0c
Lesnoy40rt77 <akejl851@gmail.com>
2021-11-04 23:40:40 +0300

    Rewrote bot structure

--- bot.py
+++ bot.py
@@ -1,55 +1,53 @@
 import os
 
+import asyncio
+import time
 import discord
 import json
 import requests
 
-# from discord.ext import commands
+from discord.ext import commands
 # второй вариант работы бота
 
-TOKEN = 'ODkzNTMzNTkzMjMyNjA5MzIw.YVc2AQ.eYfuWZl2Vmokt2QGw3iKei9rl1s'
+from config import TOKEN
 
-# bot = commands.Bot(command_prefix='!')
-bot = discord.Client()
+bot = commands.Bot(command_prefix='!')
+
+
+# bot = discord.Client()
 
 
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
+    print('Подключен как {0.user}'.format(bot))
 
-# эта команда сломалась из за следующей за ней
+
+@bot.command(aliases=['a.hug', 'anime.hug'])
+async def ahug(ctx):
+    author = ctx.message.author
+    response = requests.get('https://some-random-api.ml/animu/hug')
+    json_data = json.loads(response.text)
+
+    embed = discord.Embed(color=0xff9900, title=f'Anime for {author.mention}')
+    embed.set_image(url=json_data['link'])
+    await ctx.send(embed=embed)
+
+
 # @bot.event
 # async def on_message(message):
 #    if message.author == bot.user:
 #        return
-    # команда сделана через костыль (честно взята из статьи для теста)
-#    if message.content.startswith('!hello'):
+#
+#    if message.content.startswith('!anime_hug'):
 #        author = message.author
-#        await message.channel.send(f'Hello, {author.mention}!')
+#        response = requests.get('https://some-random-api.ml/animu/hug')
+#        json_data = json.loads(response.text)
+#
+#        embed = discord.Embed(color=0xff9900, title=f'Anime for {author.mention}')
+#        embed.set_image(url=json_data['link'])
+#        await message.channel.send(embed=embed)
+#    elif message.content.startswith('!test'):
+#        await message.channel.send('Test')
 
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith('!anime_hug'):
-        author = message.author
-        response = requests.get('https://some-random-api.ml/animu/hug')
-        json_data = json.loads(response.text)
-
-        embed = discord.Embed(color=0xff9900, title=f'Anime for {author.mention}')
-        embed.set_image(url=json_data['link'])
-        await message.channel.send(embed=embed)
-    elif message.content.startswith('!test'):
-        await message.channel.send('Test')
-
-
-# тестовая команда, при написании !hello вернет hi с пингом
-# @bot.command()
-# async def hello(ctx):
-#    author = ctx.message.author
-#    await ctx.send(f'Hello, {author.mention}!')
-# Вырезано тк не работало
 
 bot.run(TOKEN)
---
+++ config.py
@@ -0,0 +1,2 @@
+TOKEN = 'ODkzNTMzNTkzMjMyNjA5MzIw.YVc2AQ.9MsyHCddA6sIUm-yfgfIAEkF_SY'
+api_key = '9BT5EuaWEFafXHn7eLYIsjVWu'
